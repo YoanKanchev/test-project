68e47b77bbca7ee2dd8a8cc9d18ca63363ffe66b
YoanKanchev <io44@abv.bg>
2023-03-11 11:24:38 +0200

    initial commit

--- user-management-service/src/main/java/com/example/userservice/UserManagementController.java
+++ user-management-service/src/main/java/com/example/userservice/UserManagementController.java
@@ -1,2 +1,18 @@
-package com.example.userservice;public class UserManagementController {
+package com.example.userservice;
+
+@RestController
+    @RequestMapping("/users")
+public class UserManagementController {
+    public class UserController {
+        @Autowired
+        private UserService userService;
+
+        @GetMapping("/{id}")
+        public ResponseEntity<User> getUserById(@PathVariable String id) {
+            User user = userService.getUserById(id);
+            return ResponseEntity.ok(user);
+        }
+
+        // other CRUD operations
+    }
 }
